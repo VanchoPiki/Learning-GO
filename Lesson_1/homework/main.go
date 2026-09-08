5fee9aff0a6143925a547b09571210d9e612f958
VanchoPiki <varava24102020@gmail.com>
2026-09-08 22:58:24 +0300

    1st lesson complited

--- Lesson_1/homework/main.go
+++ Lesson_1/homework/main.go
@@ -1,1 +1,9 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Фильм:", "1+1\n")
+	fmt.Println("Драмма, 2013")
+	fmt.Printf("Рейтинг: %.1f | %d мин | Режиссер: %s", 9.9, 120, "Кто-то там")
+}
