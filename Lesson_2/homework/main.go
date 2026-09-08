0a21cc017e7c04a00502f84bb706898bfc754285
VanchoPiki <varava24102020@gmail.com>
2026-09-08 23:55:20 +0300

    2nd lesson complited

---
+++ Lesson_2/homework/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const (
+		price = 1000.00
+		title = "ZOV+"
+	)
+
+	var months int = 12
+	var discount = 15.0
+	total := float64(price) - float64(price)*(discount/100)
+
+	fmt.Println("===== ЧЕК GoFlix =====")
+	fmt.Printf("Тариф:%s \nМесяцев:%d \nБазовая цена:%.2f/мес. \nСкидка:%.0f%% \nИтого:%.2f\n руб./мес.", title, months, price, discount, total)
+}
