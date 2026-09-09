5c6d2e1eb771a0f27c5b1573d20abf2a99583d21
VanchoPiki <varava24102020@gmail.com>
2026-09-10 00:48:18 +0300

    Сделал урок, но не доконца ДЗ

--- Lesson_4/homework/main.go
+++ Lesson_4/homework/main.go
@@ -1,1 +1,35 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	//1
+
+	const num = 10
+
+	if num < 0 {
+		fmt.Println("Отрицательное")
+	} else if num == 0 {
+		fmt.Println("Ноль")
+	} else if num > 0 {
+
+		if num%2 == 0 {
+			fmt.Println("Четное")
+		} else if num != 0 {
+			fmt.Println("Нечетное")
+		}
+
+	}
+
+	//2
+
+	const price = 500
+	const wallet = 650
+
+	if change := wallet - price; change >= 0 {
+		fmt.Printf("Куплено, ваша сдача - %d\n\n", change)
+	} else if change < 0 {
+		fmt.Println("Не хватает денег")
+	}
+}
--- Lesson_4/lesson/main.go
+++ Lesson_4/lesson/main.go
@@ -2,16 +2,54 @@ package main
 
 import "fmt"
 
-var totalfilm = 0
-
-const zzz = 10000
-
-func addfilm() {
-	totalfilm++
-}
 func main() {
-	addfilm()
-	addfilm()
+	age := 18
 
-	fmt.Println(totalfilm)
+	if age < 14 {
+		fmt.Println("Ты еще ребенок")
+	} else if age < 18 {
+		fmt.Println("Ты подросток")
+	} else {
+		fmt.Println("Ты взрослый!")
+	}
+
+	if length := len("Hello, Go!"); length > 5 {
+		fmt.Printf("Строка длинная, в ней %d символов\n", length)
+	} else {
+		fmt.Printf("Строка короткая, всего %d символов\n", length)
+	}
+
+	day := "Суббота"
+
+	switch day {
+	case "Понедельник", "Вторник", "Среда", "Четверг", "Пятница":
+		fmt.Println("Будний день, пора работать.")
+	case "Суббота", "Воскресенье":
+		fmt.Println("Выходной! Можно отдыхать.")
+	default:
+		fmt.Println("Неизвестный день недели...")
+	}
+
+	score := 85
+
+	switch { // Условия нет!
+	case score >= 90:
+		fmt.Println("Отлично: A")
+	case score >= 75:
+		fmt.Println("Хорошо: B")
+	case score >= 60:
+		fmt.Println("Удовлетворительно: C")
+	default:
+		fmt.Println("Не сдал: F")
+	}
+
+	number := 1
+	switch number {
+	case 1:
+		fmt.Println("Один")
+		fallthrough // Принудительно передает управление в следующий case
+	case 2:
+		fmt.Println("Два (сработало из-за fallthrough!)")
+	}
+
 }
