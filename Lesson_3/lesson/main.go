6a176095da6783a19ac5c915423cd8663635a398
VanchoPiki <varava24102020@gmail.com>
2026-09-09 21:52:53 +0300

    fix main.go and update gitignore

--- Lesson_3/lesson/main.go
+++ Lesson_3/lesson/main.go
@@ -1,1 +1,39 @@
 package main
+
+import "fmt"
+
+func main() {
+	var zeroint int
+	views := 1_500_000
+
+	fmt.Println(zeroint)
+	fmt.Println(views)
+
+	var zerofloat float64
+	views2 := 85.6
+
+	fmt.Println(zerofloat)
+	fmt.Println(views2)
+
+	var zerostr string
+	movtitle := "Title"
+
+	fmt.Println(zerostr)
+	fmt.Println(movtitle)
+
+	synk := `dfsdfsdf
+dsfsdfsfdsdf
+sdfsdfsdfsdf
+sdfsdfsdf`
+
+	fmt.Println(synk)
+
+	var litlerune = 'A'
+	fmt.Println(litlerune)
+	fmt.Printf("%c\n", litlerune)
+
+	var zeroboo bool
+	isprem := true
+	nasSub := false
+	fmt.Println(zeroboo, isprem, nasSub)
+}
