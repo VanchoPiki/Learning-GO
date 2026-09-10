563ee62b6b960d6d35892714dcc155d866bb7f94
VanchoPiki <varava24102020@gmail.com>
2026-09-10 21:42:58 +0300

    3яя домашка готова

--- Lesson_4/homework/main.go
+++ Lesson_4/homework/main.go
@@ -32,4 +32,35 @@ func main() {
 	} else if change < 0 {
 		fmt.Println("Не хватает денег")
 	}
+
+	month := 4
+
+	switch month {
+	case 12, 1, 2:
+		fmt.Println("Зима")
+	case 3, 4, 5:
+		fmt.Println("Весна")
+	case 6, 7, 8:
+		fmt.Println("Лето")
+	case 9, 10, 11:
+		fmt.Println("Осень")
+	default:
+		fmt.Println("Ошибка: неверный месяц")
+	}
+
+	signal := "green"
+	pedestrians := true
+
+	switch {
+	case signal == "green" && pedestrians != true:
+		fmt.Println("Можно ехать")
+	case signal == "green" && pedestrians != false:
+		fmt.Println("Жди, пока пройдут пешеходы")
+	case signal == "yellow":
+		fmt.Println("Приготовься")
+	case signal == "red":
+		fmt.Println("Стой!")
+	default:
+		fmt.Println("Светофор сломался, движение по знакам")
+	}
 }
