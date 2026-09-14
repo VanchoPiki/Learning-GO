5795c3fe5ed1d206a10081562225db73520ec07c
VanchoPiki <varava24102020@gmail.com>
2026-09-14 10:22:22 +0300

    Выполнил ДЗ 6

--- Lesson_6/homework/main.go
+++ Lesson_6/homework/main.go
@@ -1,1 +1,44 @@
 package main
+
+import "fmt"
+
+func main() {
+	//1
+
+	//for i := 1; i <= 20; i++ {
+	//	if i%2 == 0 {
+	//		fmt.Printf("Число %d\n", i)
+	//	}
+	//}
+
+	//2
+
+	//i := 0
+	//
+	//for deposit := 10_000.0; deposit <= 20_000.0; i++{
+	//	deposit = deposit * 1.08
+	//}
+	//fmt.Println(i)
+
+	//3
+
+	//for i := 1; i <= 15; i++ {
+	//	if i%3 == 0 {
+	//		continue
+	//	}
+	//	fmt.Println(i)
+	//}
+
+	//4
+
+	num := 1
+
+	for {
+		num++
+		if num%7 == 0 && num%11 == 0 {
+			fmt.Println("Нашел число -", num)
+			break
+		}
+		fmt.Println(num)
+	}
+}
