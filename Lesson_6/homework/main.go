85b934b5afc0ef0c23833052ac91c8bbbf935f66
VanchoPiki <varava24102020@gmail.com>
2026-09-14 10:23:39 +0300

    Выполнил до конца 6 ДЗ

--- Lesson_6/homework/main.go
+++ Lesson_6/homework/main.go
@@ -5,29 +5,29 @@ import "fmt"
 func main() {
 	//1
 
-	//for i := 1; i <= 20; i++ {
-	//	if i%2 == 0 {
-	//		fmt.Printf("Число %d\n", i)
-	//	}
-	//}
+	for i := 1; i <= 20; i++ {
+		if i%2 == 0 {
+			fmt.Printf("Число %d\n", i)
+		}
+	}
 
 	//2
 
-	//i := 0
-	//
-	//for deposit := 10_000.0; deposit <= 20_000.0; i++{
-	//	deposit = deposit * 1.08
-	//}
-	//fmt.Println(i)
+	i := 0
+
+	for deposit := 10_000.0; deposit <= 20_000.0; i++{
+		deposit = deposit * 1.08
+	}
+	fmt.Println(i)
 
 	//3
 
-	//for i := 1; i <= 15; i++ {
-	//	if i%3 == 0 {
-	//		continue
-	//	}
-	//	fmt.Println(i)
-	//}
+	for i := 1; i <= 15; i++ {
+		if i%3 == 0 {
+			continue
+		}
+		fmt.Println(i)
+	}
 
 	//4
 
