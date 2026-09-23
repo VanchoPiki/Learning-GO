2e7e7fe7af5ad6e3654d1343690f2474f8afc75b
VanchoPiki <varava24102020@gmail.com>
2026-09-23 23:22:52 +0300

    Выполнил ДЗ по встраиваемым структурам, вроде так называется

--- Lesson_14/homework/main.go
+++ Lesson_14/homework/main.go
@@ -1,1 +1,36 @@
 package main
+
+import "fmt"
+
+// 1
+type SimpleWallet struct {
+	Address string
+	Balance float64
+}
+
+func (s *SimpleWallet) PrintBalance() {
+	fmt.Printf("Кошелек [%s] | Баланс: [%.2f]\n", s.Address, s.Balance)
+}
+
+type PremiumWallet struct {
+	SimpleWallet
+	CashBack float64
+}
+
+func (p *PremiumWallet) PrintBalance() {
+	fmt.Printf("[PREMIUM VIP] Кошелек [%s] | Баланс: [%.2f] | Кешбэк: [%.0f]%%\n", p.Address, p.Balance, p.CashBack*100)
+}
+
+func main() {
+	prmwal := PremiumWallet{
+		SimpleWallet{
+			"0xw123123",
+			123.4,
+		},
+		0.005,
+	}
+
+	fmt.Println(prmwal.Address)
+	prmwal.PrintBalance()
+	prmwal.SimpleWallet.PrintBalance()
+}
